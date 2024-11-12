b35c3d0e0e5da994469d7dd67f29044cf66187d4
joeribreedveld <joeri.breedveld123@gmail.com>
2024-11-12 21:52:30 +0100

    feat: make new task modal

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,5 @@
+import NewTask from "@/components/new-task";
 import TaskList from "@/components/task-list";
-import { Button } from "@/components/ui/button";
 import { TTask } from "@/lib/types";
 
 export default function Home() {
@@ -61,7 +61,7 @@ export default function Home() {
       <div className="mx-auto h-full w-full max-w-3xl md:border-x">
         <header className="flex items-center justify-between border-b px-4 py-5 sm:px-12 sm:py-8">
           <h1 className="text-lg font-semibold">Task Manager</h1>
-          <Button className="text-white">New Task</Button>
+          <NewTask />
         </header>
         <main className="px-4 pb-5 pt-5 sm:border-b sm:px-12 sm:pb-12 sm:pt-8">
           <TaskList tasks={sortedTasks} />
---
+++ src/components/new-task.tsx
@@ -0,0 +1,66 @@
+"use client";
+
+import { Button } from "@/components/ui/button";
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogFooter,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "@/components/ui/dialog";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
+import {
+  Select,
+  SelectContent,
+  SelectGroup,
+  SelectItem,
+  SelectLabel,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select";
+
+export default function NewTask() {
+  return (
+    <Dialog>
+      <DialogTrigger asChild>
+        <Button className="text-white">New Task</Button>
+      </DialogTrigger>
+      <DialogContent className="sm:max-w-sm">
+        <DialogHeader>
+          <DialogTitle className="text-base">Create Task</DialogTitle>
+          <DialogDescription>
+            Create a new task by filling out the form below.
+          </DialogDescription>
+        </DialogHeader>
+        <div className="flex flex-col gap-6 py-4">
+          <div className="flex flex-col gap-3">
+            <Label htmlFor="title">Title</Label>
+            <Input className="text-base sm:text-sm" id="title" />
+          </div>
+          <div className="flex flex-col gap-3">
+            <Label htmlFor="status">Status</Label>
+            <Select>
+              <SelectTrigger>
+                <SelectValue placeholder="Select a status" />
+              </SelectTrigger>
+              <SelectContent>
+                <SelectGroup>
+                  <SelectLabel>Status</SelectLabel>
+                  <SelectItem value="todo">Todo</SelectItem>
+                  <SelectItem value="in-progress">In Progress</SelectItem>
+                  <SelectItem value="done">Done</SelectItem>
+                </SelectGroup>
+              </SelectContent>
+            </Select>
+          </div>
+        </div>
+        <DialogFooter>
+          <Button type="submit">Submit</Button>
+        </DialogFooter>
+      </DialogContent>
+    </Dialog>
+  );
+}
